8aad88c3c7e0b58410dbd73f21e7e266756a9431
sadieff <savintsev.dima@gmail.com>
2019-12-24 15:02:49 +0300

    Доработано: обновление свойств уже существующего товара

--- 1c.php
+++ 1c.php
@@ -46,17 +46,21 @@ $requestElementCatalog  = CIBlockElement::GetList(
     array("IBLOCK_ID" => CATALOG_IBLOCK_ID), // проверка по артикулу
     false,
     false,
-    array("ID","PROPERTY_CML2_ARTICLE","TIMESTAMP_X")
+    array("ID","IBLOCK_ID",/*"PROPERTY_CML2_ARTICLE",*/"TIMESTAMP_X","PROPERTY_*")
 );
 $arrElementsCatalog = [];
 while ($elementCatalog = $requestElementCatalog -> GetNextElement()) {
     $item = $elementCatalog->GetFields();
-    $arrElementsCatalog[$item["PROPERTY_CML2_ARTICLE_VALUE"]] = array(
+    $property = $elementCatalog->GetProperties();
+    $arrElementsCatalog[$property["CML2_ARTICLE"]["VALUE"]] = array(
         "ID" => $item["ID"],
-        "ARTICUL" => $item["PROPERTY_CML2_ARTICLE_VALUE"],
+        "ARTICUL" => $property["CML2_ARTICLE"]["VALUE"],
         "TIMESTAMP_X" => $item["TIMESTAMP_X"],
+        "PROPERTIES" => $property
     );
 }
+
+//p($arrElementsCatalog);
 
 /* обойдем все элементы из 1С */
 
@@ -73,6 +77,8 @@ $request1CElements  = CIBlockElement::GetList(
 $resultCount = 0;
 $resultSection = 0;
 $resultProduct = 0;
+$resultMissed = 0;
+$resultUpdated = 0;
 
 while ($elements1C = $request1CElements -> GetNextElement()){
 
@@ -80,108 +86,134 @@ while ($elements1C = $request1CElements -> GetNextElement()){
     $item = $elements1C->GetFields();
     $prop["PROPERTIES"] = $elements1C->GetProperties();
 
+    $articul = $prop["PROPERTIES"]["CML2_ARTICLE"]["VALUE"];
     $elementBrand = createCode($prop["PROPERTIES"]["BREND"]["VALUE"]);
     $elementModel = createCode($prop["PROPERTIES"]["MODEL"]["VALUE"]);
-    if(empty($elementBrand) || empty($elementModel)) continue;
-
-    //$arr1CElements[] = array_merge($item, $prop);
+    if(empty($elementBrand) || empty($elementModel)) continue; // Если не указан бренд или модель - пропускам
 
     /* проверим, есть ли такой элемент в основном каталоге */
 
-    /*$searchElement  = CIBlockElement::GetList(
-        array("SORT" => "ASC"),
-        array("IBLOCK_ID" => CATALOG_IBLOCK_ID, "PROPERTY_CML2_ARTICLE" => $prop["PROPERTIES"]["CML2_ARTICLE"]["VALUE"]), // проверка по артикулу
-        false,
-        false,
-        array("ID")
-    );
-    if($searchElement->result->num_rows > 0) continue; /* TODO: оптимизировать - убрать запрос из цикла */
+    if(empty($arrElementsCatalog[$articul])) { // если товар НЕ существует - создаем
 
-    if(!empty($arrElementsCatalog[$prop["PROPERTIES"]["CML2_ARTICLE"]["VALUE"]])) continue; // если товар существует
+        /* определим раздел и создадим новый, если ранее такого не было */
 
-    /* определим раздел и создадим новый, если ранее такого не было */
+        if (empty($arrSectionsMap["BRANDS"][$elementBrand])) {
+            $id = $blockSection->Add(
+                Array(
+                    "ACTIVE" => "Y",
+                    "IBLOCK_SECTION_ID" => "0",
+                    "IBLOCK_ID" => CATALOG_IBLOCK_ID,
+                    "NAME" => $prop["PROPERTIES"]["BREND"]["VALUE"],
+                    "CODE" => $elementBrand,
+                )
+            );
+            $arrSectionsMap["BRANDS"][$elementBrand] = $id;
+            $resultSection++;
+        }
 
-    if (empty($arrSectionsMap["BRANDS"][$elementBrand])) {
-        $id = $blockSection->Add(
-            Array(
-                "ACTIVE" => "Y",
-                "IBLOCK_SECTION_ID" => "0",
-                "IBLOCK_ID" => CATALOG_IBLOCK_ID,
-                "NAME" => $prop["PROPERTIES"]["BREND"]["VALUE"],
-                "CODE" => $elementBrand,
-            )
+        if (empty($arrSectionsMap["MODELS"][$elementModel])){
+            $id = $blockSection->Add(
+                Array(
+                    "ACTIVE" => "Y",
+                    "IBLOCK_SECTION_ID" => $arrSectionsMap["BRANDS"][$elementBrand],
+                    "IBLOCK_ID" => CATALOG_IBLOCK_ID,
+                    "NAME" => $prop["PROPERTIES"]["MODEL"]["VALUE"],
+                    "CODE" => $elementModel,
+                )
+            );
+
+            $arrSectionsMap["MODELS"][$elementModel] = $id;
+            $resultSection++;
+            $curentSection = $id;
+        }
+        else {
+            $curentSection = $arrSectionsMap["MODELS"][$elementModel];
+        }
+
+        //echo "<div>".$item["NAME"]." - ".$curentSection."</div>";
+
+        /* добавим новый элемент  */
+
+        $arrProperty = array(
+            "TIPORAZMER_2_US" => $prop["PROPERTIES"]["TIPORAZMER_2_US"]["VALUE"], // Типоразмер
+            "TIP_TT_TL" => $prop["PROPERTIES"]["TIP_TT_TL"]["VALUE"], // Тип (TT/TL)
+            "CML2_ARTICLE" => $prop["PROPERTIES"]["CML2_ARTICLE"]["VALUE"], // Артикул
+            "CML2_MANUFACTURER" => $prop["PROPERTIES"]["CML2_MANUFACTURER"]["VALUE"], // Производитель
+            "KONSTRUKTSIYA" => $prop["PROPERTIES"]["KONSTRUKTSIYA"]["VALUE"], // Конструкция
+            "GLUBINA" => $prop["PROPERTIES"]["GLUBINA"]["VALUE"], // Глубина
+            "TIPORAZMER_1_INT" => $prop["PROPERTIES"]["TIPORAZMER_1_INT"]["VALUE"], // Типоразмер 1 (INT)
+            "TIPORAZMER_3" => $prop["PROPERTIES"]["TIPORAZMER_1_INT"]["VALUE"], // Типоразмер 3
+            "SLOYNOST" => $prop["PROPERTIES"]["SLOYNOST"]["VALUE"], // Слойность
+            "BREND" => $prop["PROPERTIES"]["BREND"]["VALUE"], // Бренд
+            "DIAMETR_POSADOCHNYY_DYUYM" => $prop["PROPERTIES"]["DIAMETR_POSADOCHNYY_DYUYM"]["VALUE"], // Диаметр посадочный (дюйм)
+            "DIAMETR_NARUZHNYY_MM" => $prop["PROPERTIES"]["DIAMETR_NARUZHNYY_MM"]["VALUE"], // Диаметр наружный (мм)
+            "SHIRINA_MM" => $prop["PROPERTIES"]["SHIRINA_MM"]["VALUE"], // Ширина (мм)
+            "MODEL" => $prop["PROPERTIES"]["MODEL"]["VALUE"], // Модель
         );
-        $arrSectionsMap["BRANDS"][$elementBrand] = $id;
-        $resultSection++;
-    }
 
-    if (empty($arrSectionsMap["MODELS"][$elementModel])){
-        $id = $blockSection->Add(
-            Array(
-                "ACTIVE" => "Y",
-                "IBLOCK_SECTION_ID" => $arrSectionsMap["BRANDS"][$elementBrand],
-                "IBLOCK_ID" => CATALOG_IBLOCK_ID,
-                "NAME" => $prop["PROPERTIES"]["MODEL"]["VALUE"],
-                "CODE" => $elementModel,
-            )
+        $arrElementProperty = array(
+            "MODIFIED_BY" => 2, // элемент изменен пользователем obmen1c
+            "IBLOCK_SECTION_ID" => $curentSection,
+            "IBLOCK_ID" => CATALOG_IBLOCK_ID,
+            "NAME" => $item["NAME"],
+            "CODE" => createCode($item["NAME"]),
+            "ACTIVE" => "Y",
+            "PREVIEW_TEXT" => $item["PREVIEW_TEXT"],
+            "PROPERTY_VALUES" => $arrProperty,
+        );
+        $product = $blockElement->Add($arrElementProperty);
+
+        $productsFields = array(
+            "ID" => $product,
+            "VAT_ID" => 1, //тип ндс
+            "VAT_INCLUDED" => "Y" //НДС входит в стоимость
+        );
+        if(CCatalogProduct::Add($productsFields)) $resultProduct++;
+
+        $resultCount++;
+
+    }
+    else { // Товар уже существует, проверим, надо ли обновить свойства
+
+        $arrProperty = array( // массив со свойствами, который проверяем
+            "TIPORAZMER_2_US", // Типоразмер
+            "TIP_TT_TL", // Тип (TT/TL)
+            "CML2_ARTICLE", // Артикул
+            "CML2_MANUFACTURER", // Производитель
+            "KONSTRUKTSIYA", // Конструкция
+            "GLUBINA", // Глубина
+            "TIPORAZMER_1_INT", // Типоразмер 1 (INT)
+            "TIPORAZMER_3", // Типоразмер 3
+            "SLOYNOST", // Слойность
+            "BREND", // Бренд
+            "DIAMETR_POSADOCHNYY_DYUYM", // Диаметр посадочный (дюйм)
+            "DIAMETR_NARUZHNYY_MM", // Диаметр наружный (мм)
+            "SHIRINA_MM", // Ширина (мм)
+            "MODEL", // Модель
         );
 
-        $arrSectionsMap["MODELS"][$elementModel] = $id;
-        $resultSection++;
-        $curentSection = $id;
+        $property = [];
+        foreach ($arrProperty as $propItem) {
+            if($arrElementsCatalog[$articul]["PROPERTIES"][$propItem]["VALUE"] == $prop["PROPERTIES"][$propItem]["VALUE"]) continue;
+            else $property[$propItem] = $prop["PROPERTIES"][$propItem]["VALUE"];
+        }
+
+        if(!empty($property)) {
+            $blockElement->SetPropertyValuesEx($arrElementsCatalog[$articul]["ID"], CATALOG_IBLOCK_ID, $property);
+            $resultUpdated++;
+        }
+        else $resultMissed++;
+
     }
-    else {
-        $curentSection = $arrSectionsMap["MODELS"][$elementModel];
-    }
 
-    echo "<div>".$item["NAME"]." - ".$curentSection."</div>";
-
-    /* добавим новый элемент  */
-
-    $arrProperty = array(
-        "TIPORAZMER_2_US" => $prop["PROPERTIES"]["TIPORAZMER_2_US"]["VALUE"], // Типоразмер
-        "TIP_TT_TL" => $prop["PROPERTIES"]["TIP_TT_TL"]["VALUE"], // Тип (TT/TL)
-        "CML2_ARTICLE" => $prop["PROPERTIES"]["CML2_ARTICLE"]["VALUE"], // Артикул
-        "CML2_MANUFACTURER" => $prop["PROPERTIES"]["CML2_MANUFACTURER"]["VALUE"], // Производитель
-        "KONSTRUKTSIYA" => $prop["PROPERTIES"]["KONSTRUKTSIYA"]["VALUE"], // Конструкция
-        "GLUBINA" => $prop["PROPERTIES"]["GLUBINA"]["VALUE"], // Глубина
-        "TIPORAZMER_1_INT" => $prop["PROPERTIES"]["TIPORAZMER_1_INT"]["VALUE"], // Типоразмер 1 (INT)
-        "TIPORAZMER_3" => $prop["PROPERTIES"]["TIPORAZMER_1_INT"]["VALUE"], // Типоразмер 3
-        "SLOYNOST" => $prop["PROPERTIES"]["SLOYNOST"]["VALUE"], // Слойность
-        "BREND" => $prop["PROPERTIES"]["BREND"]["VALUE"], // Бренд
-        "DIAMETR_POSADOCHNYY_DYUYM" => $prop["PROPERTIES"]["DIAMETR_POSADOCHNYY_DYUYM"]["VALUE"], // Диаметр посадочный (дюйм)
-        "DIAMETR_NARUZHNYY_MM" => $prop["PROPERTIES"]["DIAMETR_POSADOCHNYY_DYUYM"]["VALUE"], // Диаметр наружный (мм)
-        "SHIRINA_MM" => $prop["PROPERTIES"]["SHIRINA_MM"]["VALUE"], // Ширина (мм)
-        "MODEL" => $prop["PROPERTIES"]["MODEL"]["VALUE"], // Модель
-    );
-
-    $arrElementProperty = array(
-        "MODIFIED_BY" => 2, // элемент изменен пользователем obmen1c
-        "IBLOCK_SECTION_ID" => $curentSection,
-        "IBLOCK_ID" => CATALOG_IBLOCK_ID,
-        "NAME" => $item["NAME"],
-        "CODE" => createCode($item["NAME"]),
-        "ACTIVE" => "Y",
-        "PREVIEW_TEXT" => $item["PREVIEW_TEXT"],
-        "PROPERTY_VALUES" => $arrProperty,
-    );
-    $product = $blockElement->Add($arrElementProperty);
-
-    $productsFields = array(
-        "ID" => $product,
-        "VAT_ID" => 1, //тип ндс
-        "VAT_INCLUDED" => "Y" //НДС входит в стоимость
-    );
-    if(CCatalogProduct::Add($productsFields)) $resultProduct++;
-
-    $resultCount++;
-    //break;
 }
 
 $scriptTime = microtime(true) - $scriptTimeStart;
 
 echo "<div>Элементов всего: ".$resultCount."</div>";
-echo "<div>Товаров всего: ".$resultProduct."</div>";
+echo "<div>Товаров создано: ".$resultProduct."</div>";
+echo "<div>Товаров пропущено: ".$resultMissed."</div>";
+echo "<div>Товаров обновлено: ".$resultUpdated."</div>";
 echo "<div>Создано разделов: ".$resultSection."</div>";
 echo "<div>Время выполнения: ".$scriptTime." сек.</div>";
 
